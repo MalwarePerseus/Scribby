6ecec852919456896df36505f63cff23886d21ea
Deepak Singh <malwareperseus@gmail.com>
2022-01-18 19:34:07 +0530

    Started Hero Component

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,12 @@
 import Head from 'next/head'
 import Image from 'next/image'
-import { Navbar } from '../components/'
+import { Navbar, Hero } from '../components/'
 
 export default function Home() {
   return (
-    <div>
+    <>
       <Navbar />
-      Main
-    </div>
+      <Hero />
+    </>
   )
 }
