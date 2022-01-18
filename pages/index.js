e35333e70406cc329e65bbd82ffca472fef266d8
Deepak Singh <malwareperseus@gmail.com>
2022-01-18 21:48:07 +0530

    Home Meta Embed

--- pages/index.js
+++ pages/index.js
@@ -4,9 +4,12 @@ import { ContainerBlock, Hero } from '../components/'
 
 export default function Home() {
   return (
-      <ContainerBlock>
+      <ContainerBlock 
+        title='Scribby | Subscription Manager ' 
+        description = 'Scribby is a subscription manager which integrates all your subscriptions and lets you keep track of your use cycle to save as much as possible.'
+      >
         <Hero />
       </ContainerBlock>
       
-  )
+  ) 
 }
