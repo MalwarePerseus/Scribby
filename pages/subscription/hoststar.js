14fbf72261a81015922627dc03d4b2fb8bf9f4c8
Deepak Singh <malwareperseus@gmail.com>
2022-01-20 18:20:40 +0530

    basic starter templates

---
+++ pages/subscription/hoststar.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { ContainerBlock } from '../../components';
+
+const data = {
+    title: "Hotstar",
+    
+}
+
+function subscription() {
+    
+  return (
+    <ContainerBlock>
+        <div>
+            <h1>{data.title}</h1>
+            <div className='flex flex-row'>
+                <div className='flex-column'>
+                   <div> Hello </div>
+                   <div>DOughnut Chart</div>
+                </div>
+                <div>Bar Chart</div>    
+            </div>            
+        </div>
+    </ContainerBlock>
+    );
+}
+
+export default subscription;
