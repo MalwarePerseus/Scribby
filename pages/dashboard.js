26d7c27471fc3cdfffaf9b68ffa30452197cdcab
Deepak Singh <malwareperseus@gmail.com>
2022-01-20 05:42:12 +0530

    Initiated Dashboard

---
+++ pages/dashboard.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { ContainerBlock } from '../components';
+
+function dashboard() {
+  return(
+    <ContainerBlock> 
+        <div>
+            Dashboard
+        </div>
+    </ContainerBlock>
+  );
+}
+
+export default dashboard;
